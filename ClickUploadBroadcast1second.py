7ed61562dbdaa26f1e1b1b7c9fe70235076f4df2
ande605x <ande605x@edu.easj.dk>
2017-12-05 10:53:20 +0000

    Add files via upload

--- ClickUploadBroadcast1second.py
+++ ClickUploadBroadcast1second.py
@@ -1,15 +1,9 @@
 from RPi import GPIO
 from time import sleep
+from datetime import datetime
 import picamera
-from datetime import datetime
-
 import pyimgur
-
-BROADCAST_TO_PORT = 7000
-import time
 from socket import *
-from datetime import datetime
-
 
 clk = 15
 dt = 14
@@ -17,11 +11,18 @@ dt = 14
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-dBCounter = 40
-clkLastState = GPIO.input(clk)
 
+clkLastState = GPIO.input(clk)
+BROADCAST_TO_PORT = 7000
+dBCounter = 40
+dBSumFiveSeconds = 0
+dBAverage = 0
 countOneSecond = 0
-countOnlyTakePictureEveryFive = 5
+countFiveSeconds = 0
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
 
 def TakePicture():
     presentime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -33,7 +34,7 @@ def TakePicture():
     
     CLIENT_ID = "fc65e40de16806a"    
     im = pyimgur.Imgur(CLIENT_ID)
-    uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")  #, album="yzX2w"
+    uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")
     print(uploaded_image.title)
     print(uploaded_image.link)
     print(uploaded_image.size)
@@ -42,52 +43,50 @@ def TakePicture():
     return ImgurLink
 
 
+def SendData(link):
+    data = "Current time " + str(datetime.now()) + " dBAverage: " + str(dBAverage)
+    if len(link) > 0:
+        data = data + " Link Imgur: " + link        
+    s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+    print(data)
 
-s = socket(AF_INET, SOCK_DGRAM)
-#s.bind(('', 14593))     # (ip, port)
-# no explicit bind: will bind to default IP + random port
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-while True:
-    try:
-        while True:
-            clkState = GPIO.input(clk)
-            dtState = GPIO.input(dt)
+try:
+    while True:         
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+            
+        if clkState != clkLastState:                
+            if dtState != clkState:
+                dBCounter += 1
+            else:
+                dBCounter -= 1
+            clkLastState = clkState 
 
-            if clkState != clkLastState:
-                if dtState != clkState:
-                    dBCounter += 1
-                else:
-                    dBCounter -= 1
-                clkLastState = clkState
-                #print (dBCounter)
+        sleep(0.01)
+        countOneSecond += 1
+        if countOneSecond == 100:                        # 1 second (100 * 0.01 sleep)
+             countOneSecond = 0 
+             countFiveSeconds += 1
+             dBSumFiveSeconds += dBCounter
+             print("dBCounter: "+ str(dBCounter))
+             
+             if countFiveSeconds == 5:
+                 countFiveSeconds = 0                 
+                 dBAverage = dBSumFiveSeconds // 5
+                 dBSumFiveSeconds = 0
+                   
+                 if dBAverage >= 50:                         # over 50 dB  
+                    LinkImgur = TakePicture()
+                    SendData(LinkImgur)                   
+                 else:
+                    SendData("")
+             
+                 
 
-            sleep(0.01)
-            countOneSecond += 1
-            if countOneSecond == 100:                      # 1 second (100 milisecond)                                                            
-                if dBCounter >= 50:                        # over 50 dB
-                    if countOnlyTakePictureEveryFive == 5:  # take picture every 5th time
-                        LinkImgur = TakePicture()
-                        dataPicTaken = "Current time " + str(datetime.now()) + " dBCounter: " + str(dBCounter) + " Link Imgur: " + LinkImgur
-                        s.sendto(bytes(dataPicTaken, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-                        print(dataPicTaken)
-                        countOnlyTakePictureEveryFive = 0                        
-                    else:
-                        countOnlyTakePictureEveryFive += 1
-                        data = "Current time " + str(datetime.now()) + " dBCounter: " + str(dBCounter)
-                        s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-                        print(data)
-                        print("Waiting a little to take picture")
-                    
-
-                else:                                      # under 50 dB
-                    data = "Current time " + str(datetime.now()) + " dBCounter: " + str(dBCounter)
-                    s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-                    print(data)
-                countOneSecond = 0                        
-
-    finally:
-        GPIO.cleanup()
+finally:    
+    GPIO.cleanup()
+        
         
 
         
